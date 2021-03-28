e4d529bab52a95f90977a8bafd8e1ff1a5f37362
jezgai <josu.ezgai@gmail.com>
2021-03-28 16:50:24 +0200

    Nombres Britania Mítica - Sajones

--- javascript/mth_personaje.js
+++ javascript/mth_personaje.js
@@ -1,22 +1,13 @@
 
 class Personaje {
 	
-	constructor(genero) {
+	
+	constructor(genero, origen) {
 		this._genero = genero;
-		if (this._genero == "Masculino" ) {
-			var nombres = nombresBritanos.masculinos;
-			this._nombre = nombres[0] + "-ap-" + nombres[1];
-			this._padre = nombres[1] + "-ap-" + nombres[2];
-			nombres = nombresBritanos.femeninos;
-			this._madre = nombres[0] + "-an-" + nombres[1];
-		}
-		else {
-			var nombres = nombresBritanos.femeninos;
-			this._nombre = nombres[0] + "-an-" + nombres[1];
-			this._madre = nombres[1] + "-an-" + nombres[2];
-			nombres = nombresBritanos.masculinos;
-			this._padre = nombres[0] + "-ap-" + nombres[1];
-		}
+		if ( origen == "Britano" )
+			this.nombreBritano();
+		else
+			this.nombreSajon();
 	}
 	
 	get Nombre() {
@@ -34,5 +25,40 @@ class Personaje {
 	get Madre() {
 		return this._madre;
 	}
+	
+	nombreBritano() {
+		if (this._genero == "Masculino" ) {
+			var nombres = nombresBritanos.masculinos;
+			this._nombre = nombres[0] + "-ap-" + nombres[1];
+			this._padre = nombres[1] + "-ap-" + nombres[2];
+			nombres = nombresBritanos.femeninos;
+			this._madre = nombres[0] + "-an-" + nombres[1];
+		}
+		else {
+			var nombres = nombresBritanos.femeninos;
+			this._nombre = nombres[0] + "-an-" + nombres[1];
+			this._madre = nombres[1] + "-an-" + nombres[2];
+			nombres = nombresBritanos.masculinos;
+			this._padre = nombres[0] + "-ap-" + nombres[1];
+		}
+	}
+	
+	nombreSajon() {
+		if (this._genero == "Masculino" ) {
+			var nombres = nombresSajones.masculinos;
+			this._nombre = nombres[0] + " " + nombres[1] + "son";
+			this._padre = nombres[1] + " " + nombres[2] + "son";
+			nombres = nombresSajones.femeninos;
+			this._madre = nombres[0] + " " + nombres[1] + "dotter";
+		}
+		else {
+			var nombres = nombresSajones.femeninos;
+			this._nombre = nombres[0] + " " + nombres[1] + "dotter";
+			this._madre = nombres[1] + " " + nombres[2] + "dotter";
+			nombres = nombresSajones.masculinos;
+			this._padre = nombres[0] + " " + nombres[1] + "son";
+		}
+	}
+	
 	
 }
