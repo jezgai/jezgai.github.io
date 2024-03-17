460de1e2ad36987b795cec4ad27cdadbcfdb7f5c
jezgai <josu.ezgai@gmail.co>
2024-03-17 20:25:07 +0100

    Tarjetas de hechizos. Incluye el nivel en los hechizos que lo tienen

--- javascript/TH_plth.js
+++ javascript/TH_plth.js
@@ -43,4 +43,5 @@ class PlTH extends SistemaBase {
 		}
 		return fields;
 	}
+	
 }
--- javascript/TH_sistemabase.js
+++ javascript/TH_sistemabase.js
@@ -21,7 +21,19 @@ class SistemaBase {
 		return this.hechizos.Tipos[tipo].PDF;
 	}
     listaHechizos(tipo) {
-		return this.hechizos[tipo];
+		var i=0;
+		var lHechizos = [];
+		for (i=0; i<this.hechizos[tipo].length;i++) {
+			var hechizo = {};
+			hechizo.nombre = this.hechizos[tipo][i];
+			if ( this.hechizos.Hechizos[hechizo.nombre].hasOwnProperty('Nivel') )
+				hechizo.nivel = this.hechizos.Hechizos[hechizo.nombre].Nivel;
+			else
+				hechizo.nivel = null;
+			lHechizos.push(hechizo);
+		}
+		return lHechizos;
+		//return this.hechizos[tipo];
 	}
 	
 }
