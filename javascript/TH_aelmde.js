ae4555a9b70a397168943c5ae9c4156d37dfc190
jezgai <josu.ezgai@gmail.co>
2024-03-22 16:51:10 +0100

    Tarjetas Hechizos. Multiples pdf. Conjuros Por las tierras Humedas. Version preliminar Aventuras

--- javascript/TH_aelmde.js
+++ javascript/TH_aelmde.js
@@ -1,34 +1,45 @@
-
 class AeLMdE extends SistemaBase {
     constructor() {
         super();
         this.hechizos.EtiquetaTipos = "Listas de hechizos";
         this.hechizos.Tipos = {
-                    "Mago": { "Numero": 4, "PDF": "pdf/TM_AeLMdE_Hechizos.pdf" },
-                    "Clerigo": { "Numero": 4, "PDF": "pdf/TM_AeLMdE_Hechizos.pdf" },
-                    "Elfo": { "Numero": 4, "PDF": "pdf/TM_AeLMdE_Hechizos.pdf" },
-                    "Druida": { "Numero": 4, "PDF": "pdf/TM_AeLMdE_Hechizos.pdf" }
+                    "Mago": { "Numero": 4, "PDF": "pdf/TH_AeLMdE_Hechizos.pdf" },
+                    "Clérigo": { "Numero": 4, "PDF": "pdf/TH_AeLMdE_Hechizos.pdf" },
+                    "Elfo": { "Numero": 4, "PDF": "pdf/TH_AeLMdE_Hechizos.pdf" },
+                    "Elfo oscuro": { "Numero": 4, "PDF": "pdf/TH_AeLMdE_Hechizos.pdf" },
+                    "Paladín": { "Numero": 4, "PDF": "pdf/TH_AeLMdE_Hechizos.pdf" },
+                    "Druida": { "Numero": 4, "PDF": "pdf/TH_AeLMdE_Hechizos.pdf" }
                 };
         this.hechizos.Mago = [ ];
-        this.hechizos.Clerigo = [ ];
+        this.hechizos["Clérigo"] = [ ];
         this.hechizos.Elfo = [ ];
         this.hechizos.Druida = [ ];
+        this.hechizos["Elfo oscuro"] = [ ];
+        this.hechizos["Paladín"] = [ ];
         
-        /*var tablas = [ {} ];
-        Comun.cargatablas("TH-Aventuras-en-La-Marca-del-Este.json", this);*/
+        Comun.cargatablas("TH-Aventuras-en-LMdE.json", this);
     }
     
     init(tablas) {
         this.hechizos.Hechizos = tablas.Hechizos;
         this.hechizos.Mago = tablas.Mago;
-        this.hechizos.Clerigo = tablas.Clerigo;
+        this.hechizos["Clérigo"] = tablas["Clérigo"];
         this.hechizos.Elfo = tablas.Elfo;
         this.hechizos.Druida = tablas.Druida;
+        this.hechizos["Elfo oscuro"] = tablas["Elfo oscuro"];
+        this.hechizos["Paladín"] = tablas["Paladín"];
     }
     
 	rellenaPDF(lHechizos) {
 		var i=0;
-		var fields = {};
+		var fields = {};		
+		for (i=0;i<lHechizos.length;i++) {
+            fields["Nombre" + (i+1)] = [ lHechizos[i] ];
+            fields["Descripcion" + (i+1) ] =  [ this.hechizos.Hechizos[lHechizos[i]].Descripción ];
+			fields["Alcance" + (i+1) ] = [ this.hechizos.Hechizos[lHechizos[i]].Alcance ];
+			fields["Duracion" + (i+1) ] = [ this.hechizos.Hechizos[lHechizos[i]]["Duración"] ];
+			fields["Nivel" + (i+1) ] = [ this.hechizos.Hechizos[lHechizos[i]].Nivel ];
+		}
 		return fields;
 	}
 }
