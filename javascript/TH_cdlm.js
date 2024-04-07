f488a443f114ccbee59067d819351fcf64f8e362
jezgai <josu.ezgai@gmail.com>
2024-04-07 21:07:59 +0200

    Tarjetas Hechizos. Version preliminar de Cronicas de la Marca

--- javascript/TH_cdlm.js
+++ javascript/TH_cdlm.js
@@ -4,18 +4,18 @@ class CdLM extends SistemaBase {
         super();
         this.hechizos.EtiquetaTipos = "Listas de hechizos";
         this.hechizos.Tipos = {
-                    "Mago": { "Numero": 4, "PDF": "pdf/TM_CdLM_Hechizos.pdf" },
-                    "Clerigo": { "Numero": 4, "PDF": "pdf/TM_CdLM_Hechizos.pdf" },
-                    "Ilusionista": { "Numero": 4, "PDF": "pdf/TM_CdLM_Hechizos.pdf" },
-                    "Druida": { "Numero": 4, "PDF": "pdf/TM_CdLM_Hechizos.pdf" }
+                    "Mago": { "Numero": 4, "PDF": "pdf/TH_CdLM_Hechizos.pdf" },
+                    "Clerigo": { "Numero": 4, "PDF": "pdf/TH_CdLM_Hechizos.pdf" },
+                    "Ilusionista": { "Numero": 4, "PDF": "pdf/TH_CdLM_Hechizos.pdf" },
+                    "Druida": { "Numero": 4, "PDF": "pdf/TH_CdLM_Hechizos.pdf" }
                 };
         this.hechizos.Mago = [ ];
         this.hechizos.Clerigo = [ ];
         this.hechizos.Ilusionista = [ ];
         this.hechizos.Druida = [ ];
         
-        /*var tablas = [ {} ];
-        Comun.cargatablas("TH-Cronicas-de-La-Marca.json", this);*/
+        
+        Comun.cargatablas("TH-Cronicas-de-La-Marca.json", this);
     }
         
     init(tablas) {
@@ -28,7 +28,18 @@ class CdLM extends SistemaBase {
     
 	rellenaPDF(lHechizos) {
 		var i=0;
-		var fields = {};
+		var fields = {};		
+		for (i=0;i<lHechizos.length;i++) {
+            fields["Nombre" + (i+1)] = [ lHechizos[i] ];
+            fields["Descripcion" + (i+1) ] =  [ this.hechizos.Hechizos[lHechizos[i]].Descripción ];
+			fields["Alcance" + (i+1) ] = [ this.hechizos.Hechizos[lHechizos[i]].Alcance ];
+			fields["Duracion" + (i+1) ] = [ this.hechizos.Hechizos[lHechizos[i]]["Duración"] ];			
+			fields["TS" + (i+1) ] = [ this.hechizos.Hechizos[lHechizos[i]].TS ];			
+			fields["TL" + (i+1) ] = [ this.hechizos.Hechizos[lHechizos[i]].TL ];
+			fields["Componentes" + (i+1) ] = [ this.hechizos.Hechizos[lHechizos[i]].Componentes ];			
+			fields["RC" + (i+1) ] = [ this.hechizos.Hechizos[lHechizos[i]].RC ];
+			fields["Nivel" + (i+1) ] = [ this.hechizos.Hechizos[lHechizos[i]].Nivel ];
+		}
 		return fields;
 	}
 }
