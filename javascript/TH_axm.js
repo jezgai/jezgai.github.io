619c4a4a8922ffff9e3957cc46f67c1fd828cf3d
jezgai <josu.ezgai@gmail.com>
2024-04-28 21:25:45 +0200

    Tarjetas Hechizos. Axis Mundi. Incluir nivel en filtro y algún conjuro más

--- javascript/TH_axm.js
+++ javascript/TH_axm.js
@@ -18,7 +18,62 @@ class AxM extends SistemaBase {
         this.hechizos.Arcana = tablas.Arcana;
         this.hechizos.Divina = tablas.Divina;
     }
+        
+    listaHechizosTipoNivel(tipo, nivel) {
+		var i=0;
+		var lHechizos = [];
+		
+		if ( tipo == "" ) {
+			return lHechizos;
+		}
+		var listaHechizosAux = this.listaHechizos(tipo);
+		if ( nivel == "" ) {
+			return listaHechizosAux;
+		}
+		var nivelint = parseInt(nivel);
+		
+		for (i=0; i<listaHechizosAux.length; i++) {
+			if ( listaHechizosAux[i].nivel == nivelint )
+			{
+				var hechizo = {};
+				hechizo.nombre = listaHechizosAux[i].nombre;
+				hechizo.nivel = nivelint;
+				lHechizos.push(hechizo);
+			}
+		}
+		return lHechizos;
+	}
+	
+	cargaHechizos() {
+	        lHechizos = this.listaHechizosTipoNivel(this.tiposconjuros[0], this.tiposconjuros[1]);
+	        this.pintaHechizos(lHechizos);
+    	    return lHechizos;
+	}
+	
     
+    listaTipos() {
+		return [ Object.keys(this.hechizos.Tipos), [ "1", "2", "3", "4", "5", "6", "7", "8", "9" ] ];
+    }
+    
+    
+    construyeSelectTipos(tipossistema) {
+		var i=0;
+		var tipos = tipossistema[0];
+		var niveles = tipossistema[1];
+		var tiposhtml = "<div class='mitad'>	<label class='w3-text-blue'><strong>" + this.etiquetaTipos() + "</strong></label><select class='w3-select' name='tipossistema' onChange='seleccionatipo(this, 0)'><option>--Selecciona--</option>";
+		for (i=0; i<tipos.length; i++) {
+			tiposhtml += '<option>' + tipos[i] + '</option>';
+		}
+		tiposhtml += '</select></div>';
+		
+		
+		tiposhtml += "<div class='mitad'>	<label class='w3-text-blue'><strong>Nivel</strong></label><select class='w3-select' name='niveles' onChange='seleccionatipo(this, 1)'><option>--Selecciona--</option>";
+		for (i=0; i<niveles.length; i++) {
+			tiposhtml += '<option>' + niveles[i] + '</option>';
+		}
+		tiposhtml += '</select></div>';
+		return tiposhtml;
+	}
 
 	rellenaPDF(lHechizos) {
 		var i=0;
