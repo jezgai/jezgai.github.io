dcfde0e3f17b1861753335fcd4850fb183956efe
jezgai <josu.ezgai@gmail.com>
2025-01-30 21:04:18 +0100

    Se oculta temporalmente DH para las tarjetas de hechizos

--- javascript/TH_sistemas.js
+++ javascript/TH_sistemas.js
@@ -1,14 +1,14 @@
 
 class Sistemas {
     constructor() {
-		this.nombressistemas = [ "", "Por las Tierras Hundidas", "Aventuras en La Marca del Este", "Cronicas de la Marca", "Axis Mundi", "Dungeon Hack", "Tierras Quebradas" ];
+		this.nombressistemas = [ "", "Por las Tierras Hundidas", "Aventuras en La Marca del Este", "Cronicas de la Marca", "Axis Mundi", "Tierras Quebradas", "Dungeon Hack" ];
         this.sistemas = new Map();
         this.sistemas.set(this.nombressistemas[1], new PlTH());
         this.sistemas.set(this.nombressistemas[2], new AeLMdE());
         this.sistemas.set(this.nombressistemas[3], new CdLM());
         this.sistemas.set(this.nombressistemas[4], new AxM());
-        this.sistemas.set(this.nombressistemas[5], new DH());
-        this.sistemas.set(this.nombressistemas[6], new TQuebradas());
+        this.sistemas.set(this.nombressistemas[5], new TQuebradas());
+        this.sistemas.set(this.nombressistemas[6], new DH());
     }
     
     getSistema(_sistema) {
