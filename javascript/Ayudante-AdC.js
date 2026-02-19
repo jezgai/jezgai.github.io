68c3ae4d83bd8283f9c0cb335a249d7a5c5bacb7
jezgai <josu.ezgai@gmail.com>
2026-02-19 21:34:28 +0100

    Ayudante del Amo del Calabozo. Reorganizacion del menu

--- javascript/Ayudante-AdC.js
+++ javascript/Ayudante-AdC.js
@@ -9,32 +9,51 @@ function closeNav() {
   document.getElementById("main").style.marginLeft= "0";
 }
 
+function enlaceSeccion(nbseccion, titulo) {
+    return "<a href='javascript:void(0)' onclick='textos.muestraTexto(" + '"' + nbseccion + '"' + ")'>" + titulo + "</a>";
+}
+    
+
 class Textos {
     constructor() {
        this.textos = {
-            AcercaDe: { titulo: "", texto: "" },
-            Glosario: { titulo: "", texto: "" },
-            CreacionPJ: { titulo: "", texto: "" },
-            Sistema: { titulo: "", texto: "" },
-            Velocidad: { titulo: "", texto: "" },
-            Descansos: { titulo: "", texto: "" },
-            Combate: { titulo: "", texto: "" },
-            Daño: { titulo: "", texto: "" },
-            Curacion: { titulo: "", texto: "" },
-            Persecuciones: { titulo: "", texto: "" },
-            Tiempo: { titulo: "", texto: "" },
-            Trampas: { titulo: "", texto: "" },
-            Luz: { titulo: "", texto: "" }
+            AcercaDe: { titulo: "", texto: "", referencias: [] },
+            Glosario: { titulo: "", texto: "", referencias: [] },
+            CreacionPJ: { titulo: "", texto: "", referencias: [] },
+            Sistema: { titulo: "", texto: "", referencias: [] },
+            Velocidad: { titulo: "", texto: "", referencias: [] },
+            Descansos: { titulo: "", texto: "", referencias: [] },
+            Combate: { titulo: "", texto: "", referencias: [] },
+            Daño: { titulo: "", texto: "", referencias: [] },
+            Curacion: { titulo: "", texto: "", referencias: [] },
+            Persecuciones: { titulo: "", texto: "", referencias: [] },
+            Tiempo: { titulo: "", texto: "", referencias: [] },
+            Trampas: { titulo: "", texto: "", referencias: [] },
+            Luz: { titulo: "", texto: "", referencias: [] }
        };
     }
     
-    
-    muestraTexto(texto) {
-       document.getElementById("seccion").innerHTML=this.textos[texto].titulo;
-       document.getElementById("contenido").innerHTML=this.textos[texto].texto;
-       closeNav();
+    referencias(texto) {
+        if ( this.textos[texto].referencias.length > 0 ) {
+            var i=0;
+            var aux="";
+            for (i=0;i<this.textos[texto].referencias.length;i++) {
+                aux += " " + enlaceSeccion(this.textos[texto].referencias[i], this.textos[this.textos[texto].referencias[i]].titulo);
+            }
+            if (aux != "") {
+                aux = "<br/><br/><u>Referido por</u>:" + aux;
+                return aux;
+            }
+        }
+        return "";
     }
     
+    muestraTexto(texto) {
+       document.getElementById("seccion").innerHTML="<h2>" + this.textos[texto].titulo + "</h2>";
+       document.getElementById("contenido").innerHTML=this.textos[texto].texto + this.referencias(texto);
+       closeNav();
+    }
+        
     parsea(textoclave) {
         var texto = this.textos[textoclave].texto;
         var iniciomarca = this.textos[textoclave].texto.indexOf("[[");
@@ -43,7 +62,7 @@ class Textos {
             if (finmarca != -1 ) {
                 var marca = this.textos[textoclave].texto.substring(iniciomarca+2,finmarca);
                 if ( this.textos.hasOwnProperty(marca) ) {
-                    texto = texto.replace(this.textos[textoclave].texto.substring(iniciomarca,finmarca+2), "<a href='javascript:void(0)' onclick='textos.muestraTexto(" + '"' + marca + '"' + ")'>" + this.textos[marca].titulo + "</a>");
+                    texto = texto.replace(this.textos[textoclave].texto.substring(iniciomarca,finmarca+2), enlaceSeccion(marca, this.textos[marca].titulo));
                 }
             }
             iniciomarca =  this.textos[textoclave].texto.indexOf("[[", iniciomarca+2);
